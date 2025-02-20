6d3103740f059d59425729f6003a18f31453f883
hasan <hsnckel19@gmail.com>
2025-02-20 13:16:13 +0300

    4.12-4.15 tamamlandı

--- harici.js
+++ harici.js
@@ -1,1 +1,18 @@
-alert("harici dosya...");
+//tek satır açıklama
+alert("harici dosya..." );
+//değişken oluşturma
+var a = 1 , b = 2; 
+//değişkenin hiçbir zaman değişmeyecek olan değerler için const komutu kullanılır.
+const pi = 3.14;
+//değişkeni sadece tanımladığı fonksiyon içinde kullanacaksanız let komutu kullanılır.
+let y=2, x=1;
+/*değişken tanımlama kuaralları:
+ 1-for , if gibi komutlarını değişken olarak tanımlayamayız.
+ 2- değişkenlerin ilk karakteri harf olmak zorundadır.
+ 3- iki kelime arasında boşluk asla kullanılmaz.
+ 4-string değerler her zaman "" ile kullanılır.
+ 5-büyük-küçük haf duyarlığı vardır.
+ 6-türkçe harfler kullanılabilir. ama sorun yaşayabilme şansınız vardır.
+ 7-
+ 
+*/ 

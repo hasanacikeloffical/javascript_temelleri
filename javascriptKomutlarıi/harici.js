fd90948d317adfa40b76110e1b7855b346688257
hasan <hsnckel19@gmail.com>
2025-02-25 23:42:54 +0300

    tamamlandı

--- javascriptKomutlarıi/harici.js
+++ javascriptKomutlarıi/harici.js
@@ -13,6 +13,42 @@ let y=2, x=1;
  4-string değerler her zaman "" ile kullanılır.
  5-büyük-küçük haf duyarlığı vardır.
  6-türkçe harfler kullanılabilir. ama sorun yaşayabilme şansınız vardır.
- 7-
+ 7- */
  
-*/ 
+ 
+// yapılan işlem sonuçlarını ekranda görmek için 1-document.write() 2-alert() 3-console.log() 4-innerHTML komutlarıdır.
+//1-
+/* alert("birinci yöntem");
+var ad = "hasan";
+alert(ad); */ 
+
+
+ 
+//2-
+var ad = "hasan";
+var soyad = "acikel";
+document.write("ad: ",ad); 
+
+//3-
+console.log(ad);
+
+//4- bu komut html dosyasında bulunan id yi değiştirmemizi sağlar.
+document.getElementById("test").innerHTML = soyad; 
+
+//ziyaretçiden veri almak için html formları veya prompt() komutu kullanılır.
+ var no = prompt("numaranız: ");
+ document.write("no" ,no);
+ alert(no); 
+ console.log(no);
+
+
+ 
+
+
+
+
+
+
+
+
+ 
